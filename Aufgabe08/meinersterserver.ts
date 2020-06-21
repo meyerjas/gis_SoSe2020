128288ba89fc6e5e218912f601721df6d0806a8b
Jasmin Meyer <meyer.jasmin1099@gmail.com>
2020-06-21 14:05:37 +0200

    a08

---
+++ Aufgabe08/meinersterserver.ts
@@ -0,0 +1,21 @@
+namespace aufgabe08 {
+
+    document.getElementById("button")?.addEventListener("click", handleData);
+
+    async function handleData(): Promise<void>  {
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: string = "https://gisjas.herokuapp.com/";
+        let query: URLSearchParams = new URLSearchParams(<any> formData);
+        url = url + "?" + query.toString();
+
+        let response: Response = await fetch(url);
+        let response2: string = await response.text();
+        console.log(response2);
+
+    }
+
+
+
+
+
+}

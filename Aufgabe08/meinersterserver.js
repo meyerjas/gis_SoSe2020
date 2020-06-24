0f4f04833f575164a9f65b4099c563d635ff92cb
Jasmin Meyer <meyer.jasmin1099@gmail.com>
2020-06-24 09:17:20 +0200

    watch ts

--- Aufgabe08/meinersterserver.js
+++ Aufgabe08/meinersterserver.js
@@ -1,2 +1,15 @@
 "use strict";
+var aufgabe08;
+(function (aufgabe08) {
+    document.getElementById("button")?.addEventListener("click", handleData);
+    async function handleData() {
+        let formData = new FormData(document.forms[0]);
+        let url = "https://gisjas.herokuapp.com";
+        let query = new URLSearchParams(formData);
+        url = url + "?" + query.toString();
+        let response = await fetch(url);
+        let ausgabe = await response.text();
+        console.log(ausgabe);
+    }
+})(aufgabe08 || (aufgabe08 = {}));
 //# sourceMappingURL=meinersterserver.js.map

07a5ccdb6b5594def0c5feaf296a2fe740548452
Jasmin Meyer <meyer.jasmin1099@gmail.com>
2020-06-28 11:15:31 +0200

    js

--- Aufgabe08/server.js
+++ Aufgabe08/server.js
@@ -1,26 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.A08Server = void 0;
-const Http = require("http");
+const Http = require("http"); //http - Modul laden mit allen Finktionalitäten
 var A08Server;
 (function (A08Server) {
-    console.log("Starting server");
-    let port = Number(process.env.PORT);
+    console.log("Starting server"); // Gibt in der Konsole aus "Starting Server"
+    let port = Number(process.env.PORT); //Sucht ein Port
     if (!port)
-        port = 8100;
-    let server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
-    server.listen(port);
+        port = 8100; //Setzt den Port auf 8100, wenn er nichts findet
+    let server = Http.createServer(); //Server erschaffen
+    server.addListener("request", handleRequest); //EventListener für requests erschaffen
+    server.addListener("listening", handleListen); //Eventlistener fürs zuhören schaffen
+    server.listen(port); //Server hört auf den Port
     function handleListen() {
-        console.log("Listening");
+        console.log("Listening"); //wenn die funktion handleListen ausgeführt wird, gibt die Konsole "Listening" aus
     }
     function handleRequest(_request, _response) {
-        console.log("I hear voices!");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
-        _response.end();
+        console.log("I hear voices!"); //wenn die funktion handleRequest ausgeführt wird, gibt die Konsole "I hear voices!" aus.
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //Antwort ist vom typ Text
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //Alle können auf die Antwort zugreifen
+        _response.write(_request.url); //URL wird geschrieben
+        console.log("Ausgabe: " + _request.url);
+        _response.end(); //Antwort wird an den Client geschickt
     }
 })(A08Server = exports.A08Server || (exports.A08Server = {}));
 //# sourceMappingURL=server.js.map

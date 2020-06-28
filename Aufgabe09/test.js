c76b82757be4f32962e133eb5f28b5a11e7fc4a0
Jasmin Meyer <meyer.jasmin1099@gmail.com>
2020-06-28 15:32:04 +0200

    test.ts änderungen

--- Aufgabe09/test.js
+++ Aufgabe09/test.js
@@ -18,31 +18,32 @@ var A09Server;
     }
     function handleRequest(_request, _response) {
         console.log("I hear voices!");
-        let adresse = "http://localhost:8080/default.htm/ordnerstruktur/blabla?jahr=2017&monat=february";
+        //let adresse: string = "http://localhost:8080/default.htm/ordnerstruktur/blabla?jahr=2017&monat=february";
         //Adresse parsen (umwandeln):
         //tslint:disable-next-line: no-any
-        let q = Url.parse(adresse, true);
-        /*Die parse Methode gibt ein Objekt zurück, dass die URL Eigenschaften enthält. So können die fest definierten Eigenschaften einer URL ausgelesen werden:*/
+        //let q: any = Url.parse(adresse, true);
+        /*Die parse Methode gibt ein Objekt zurück, dass die URL Eigenschaften enthält. So können die fest definierten Eigenschaften einer URL ausgelesen werden:
         console.log(q.host);
         console.log(q.pathname);
         console.log(q.search);
+        */
         /*Die query Eigenschaft gibt ein Ojekt zurück, dass alle query-string Parameter als Eigenschaften besitzt. So können beliebig gesendete Attribute ausgelesen werden:*/
         //tslint:disable-next-line: no-any
-        var qdata = q.query;
-        console.log(qdata.monat);
+        //var qdata: any = q.query;
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         if (_request.url) {
             let url = Url.parse(_request.url, true);
-            console.log(url);
-            for (let key in url.query) {
-                _response.write(key + ": " + url.query[key] + "</br>");
+            if (url.pathname == "/html") {
+                for (let key in url.query) {
+                    _response.write(key + ": " + url.query[key] + "<br>");
+                }
             }
-            let jsonString = JSON.stringify(url.query);
-            _response.write(jsonString);
+            if (url.pathname == "/json") {
+                let jsonString = JSON.stringify(url.query);
+                _response.write(jsonString);
+            }
         }
-        _response.write(_request.url);
-        _response.write("Hallo ich bin eine response.");
         _response.end();
     }
 })(A09Server = exports.A09Server || (exports.A09Server = {}));
--- Aufgabe09/test.ts
+++ Aufgabe09/test.ts
@@ -18,20 +18,21 @@ export namespace A09Server {
 
   function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
     console.log("I hear voices!");
-    let adresse: string = "http://localhost:8080/default.htm/ordnerstruktur/blabla?jahr=2017&monat=february";
+    //let adresse: string = "http://localhost:8080/default.htm/ordnerstruktur/blabla?jahr=2017&monat=february";
     //Adresse parsen (umwandeln):
     //tslint:disable-next-line: no-any
-    let q: any = Url.parse(adresse, true);
+    //let q: any = Url.parse(adresse, true);
 
-    /*Die parse Methode gibt ein Objekt zurück, dass die URL Eigenschaften enthält. So können die fest definierten Eigenschaften einer URL ausgelesen werden:*/
+    /*Die parse Methode gibt ein Objekt zurück, dass die URL Eigenschaften enthält. So können die fest definierten Eigenschaften einer URL ausgelesen werden:
     console.log(q.host);
     console.log(q.pathname);
     console.log(q.search);
+    */
 
     /*Die query Eigenschaft gibt ein Ojekt zurück, dass alle query-string Parameter als Eigenschaften besitzt. So können beliebig gesendete Attribute ausgelesen werden:*/
     //tslint:disable-next-line: no-any
-    var qdata: any = q.query;
-    console.log(qdata.monat);
+    //var qdata: any = q.query;
+    
 
     _response.setHeader("content-type", "text/html; charset=utf-8");
     _response.setHeader("Access-Control-Allow-Origin", "*");
@@ -39,16 +40,18 @@ export namespace A09Server {
 
     if (_request.url) {
       let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
-      console.log(url);
-      for (let key in url.query) {
-        _response.write(key + ": " + url.query[key] + "</br>");
+      
+      if (url.pathname == "/html") {
+        for (let key in url.query) {
+          _response.write(key + ": " + url.query[key] + "<br>"); 
       }
-      let jsonString: string = JSON.stringify(url.query);
-      _response.write(jsonString);
+    }
+      if (url.pathname == "/json") {
+        let jsonString: string = JSON.stringify(url.query);
+        _response.write(jsonString);
+      }
     }
 
-    _response.write(_request.url);
-    _response.write("Hallo ich bin eine response.");
     _response.end();
   }
 }
